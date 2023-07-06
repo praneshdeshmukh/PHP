1d688a6a477a4be7df57975e6a93e6994ceef1bc
Pranesh Deshmukh <praneshd2003@gmail.com>
2023-07-06 23:45:31 +0530

    Create help.php

---
+++ Shopping Cart using PHP and XAMPP /help.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>help</title>
+    <style>
+                .help-section {
+          background-color: #f8f8f8;
+          padding: 40px 0;
+          text-align: center;
+        }
+        
+        .help-section h2 {
+          margin-bottom: 20px;
+        }
+        
+        .help-items {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: center;
+        }
+        
+        .help-item {
+          width: 250px;
+          margin: 0 20px;
+          padding: 20px;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+          text-align: center;
+        }
+        
+        .help-item h3 {
+          margin-bottom: 10px;
+        }
+        
+        .help-item p {
+          color: #666;
+          margin-bottom: 15px;
+        }
+        
+        .help-item a {
+          background-color: #333;
+          color: #fff;
+          display: inline-block;
+          padding: 10px 20px;
+          border-radius: 4px;
+          text-decoration: none;
+        }
+        
+    </style>
+</head>
+<body>
+<section class="help-section">
+  <div class="container">
+    <h2>Help & Support</h2>
+    <div class="help-items">
+      <div class="help-item">
+        <h3>FAQs</h3>
+        <p>Find answers to commonly asked questions in our FAQs section.</p>
+        <a href="#">Read FAQs</a>
+      </div>
+      <div class="help-item">
+        <h3>Contact Us</h3>
+        <p>Reach out to our customer support team for assistance.</p>
+        <a href="contact.html">Contact Us</a>
+      </div>
+      <div class="help-item">
+        <h3>Shipping & Delivery</h3>
+        <p>Learn about our shipping and delivery policies.</p>
+        <a href="#">Shipping Info</a>
+      </div>
+      <div class="help-item">
+        <h3>Returns & Refunds</h3>
+        <p>Find out how to return products and get refunds.</p>
+        <a href="#">Returns Policy</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+</body>
+</html>
